bbd91c2c6c20d4dc78c3c6c6a8f36114f0bb3e36
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-08 15:44:32 -0700

    classNames for conditional className on btn

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 import 'components/Button.scss';
+import classNames from 'classnames';
 
 export default function Button(props) {
-  let buttonClass = 'button';
+  // let buttonClass = 'button';
 
-  if (props.confirm) {
-    buttonClass += ' button--confirm';
-  }
+  // if (props.confirm) {
+  //   buttonClass += ' button--confirm';
+  // }
 
-  if (props.danger) {
-    buttonClass += ' button--danger';
-  }
+  // if (props.danger) {
+  //   buttonClass += ' button--danger';
+  // }
+
+  let buttonClass = classNames(
+    'button',
+    { 'button--confirm': props.confirm },
+    { 'button--danger': props.danger }
+  );
 
   return (
     <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>
