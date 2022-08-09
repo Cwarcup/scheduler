b8436a4be4089977601f0cc4d62b93ceecf31016
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-09 13:28:06 -0700

    InterviewerList created with Stories

---
+++ src/components/InterviewerList.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import '../styles/InterviewerList.scss';
+import InterviewerListItem from './InterviewerListItem';
+
+function InterviewerList(props) {
+  //build an array and insert InterviewerListItem for each interviewer
+  const interviewerArr = props.interviewers.map((int) => {
+    return (
+      <InterviewerListItem
+        key={int.id}
+        id={int.id}
+        name={int.name}
+        avatar={int.avatar}
+        setInterviewer={props.setInterviewer}
+        selected={int.id === props.interviewer}
+      />
+    );
+  });
+
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewerArr}</ul>
+    </section>
+  );
+}
+
+export default InterviewerList;
