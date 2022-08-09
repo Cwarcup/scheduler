afd7b42dd17af6785e07a55d07b5a912fa562ccf
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-08 17:49:01 -0700

    Day 2 complete

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import { useState } from 'react';
 import 'components/Application.scss';
 import DayList from './DayList';
 
@@ -22,13 +22,15 @@ const days = [
 ];
 
 export default function Application(props) {
+  const [day, setDay] = useState('Monday');
+
   return (
     <main className="layout">
       <section className="sidebar">
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={'Monday'} setDay={(day) => console.log(day)} />
+          <DayList days={days} day={day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
