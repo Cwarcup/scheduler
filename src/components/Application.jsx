235bdca33461fe311e52478ef0f3e3236822e41a
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-10 14:39:33 -0700

    getAppointmentsForDay to display appts

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -6,45 +6,7 @@ import "../styles/Application.scss";
 import DayList from "./DayList.jsx";
 import Appointment from "components/Appointment";
 import axios from "axios";
-
-// const appointments = {
-//   1: {
-//     id: 1,
-//     time: "12pm",
-//   },
-//   2: {
-//     id: 2,
-//     time: "1pm",
-//     interview: {
-//       student: "Lydia Miller-Jones",
-//       interviewer: {
-//         id: 3,
-//         name: "Sylvia Palmer",
-//         avatar: "https://i.imgur.com/LpaY82x.png",
-//       },
-//     },
-//   },
-//   3: {
-//     id: 3,
-//     time: "2pm",
-//   },
-//   4: {
-//     id: 4,
-//     time: "3pm",
-//     interview: {
-//       student: "Archie Andrews",
-//       interviewer: {
-//         id: 4,
-//         name: "Cohana Roy",
-//         avatar: "https://i.imgur.com/FK8V841.jpg",
-//       },
-//     },
-//   },
-//   5: {
-//     id: 5,
-//     time: "4pm",
-//   },
-// };
+import { getAppointmentsForDay } from "../helpers/selectors.jsx";
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -78,7 +40,7 @@ export default function Application(props) {
       .catch((err) => console.log(err));
   }, []);
 
-  const dailyAppointments = [];
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
   // create array of appointments for the day
   const appointmentArr = dailyAppointments.map((app) => {
     return <Appointment key={app.id} {...app} />;
