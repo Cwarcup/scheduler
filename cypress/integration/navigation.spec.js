f8aa67a2045cd37d49a80c7e97646133b6495a48
cwarcup <88569957+Cwarcup@users.noreply.github.com>
2022-08-17 14:13:20 -0700

    first test with cypress

---
+++ cypress/integration/navigation.spec.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-undef */
+// describe("My first Cypress tests", () => {
+
+// 	beforeEach(() => {
+// 		cy.visit("/");
+// 	})
+
+//   it("true is true", () => {
+//     assert.equal(true, true);
+//   });
+
+// 	it('can visit a web page', () => {
+// 		cy.visit('https://example.cypress.io')
+
+// 		cy.get('.nav-link')
+// 		.first()
+// 		.find('input')
+// 		.uncheck()
+// 		.should('not.be.checked')
+// 	})
+// });
+describe("Navigation", () => {
+  it("should visit root", () => {
+    cy.visit("/");
+  });
+
+  it("should navigate to Tuesday", () => {
+    cy.visit("/");
+    cy.get("li").contains("Tuesday").click();
+
+    cy.contains("[data-testid=day]", "Tuesday")
+      .click()
+      .should("have.class", "day-list__item--selected");
+  });
+});
